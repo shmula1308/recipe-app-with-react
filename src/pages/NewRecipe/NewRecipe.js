01c3c6bb58fc0e0a66165eba57a1d9475fb063af
Shpend Mula <shmula@hotmail.com>
2021-10-26 21:24:05 +0200

    Set up a counter

--- src/pages/NewRecipe/NewRecipe.js
+++ src/pages/NewRecipe/NewRecipe.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const NewRecipe = () => {
-  return <h1 className='title'>New Recipe</h1>;
+  const [counter, setCounter] = useState(0);
+  
+  const decrementhandler = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+  const incrementhandler = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  return (
+    <div>
+      <h1 className='title'>New Recipe</h1>
+      <button onClick={decrementhandler}>-</button>
+      <span>{counter}</span>
+      <button onClick={incrementhandler}>+</button>
+    </div>
+  );
 };
 
 export default NewRecipe;
