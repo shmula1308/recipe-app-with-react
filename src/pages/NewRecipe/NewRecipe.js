8e4445455bc275cfbfec05ce70d283a7e35c72fa
Shpend Mula <shmula@hotmail.com>
2021-10-27 16:00:58 +0200

    minor fix

--- src/pages/NewRecipe/NewRecipe.js
+++ src/pages/NewRecipe/NewRecipe.js
@@ -1,21 +1,7 @@
-import { useState } from "react";
-
 const NewRecipe = () => {
-  const [counter, setCounter] = useState(0);
-  
-  const decrementhandler = () => {
-    setCounter((prevCounter) => prevCounter - 1);
-  };
-  const incrementhandler = () => {
-    setCounter((prevCounter) => prevCounter + 1);
-  };
-
   return (
     <div>
       <h1 className='title'>New Recipe</h1>
-      <button onClick={decrementhandler}>-</button>
-      <span>{counter}</span>
-      <button onClick={incrementhandler}>+</button>
     </div>
   );
 };
