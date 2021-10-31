3371c0497a565170598ae1398e7b90c3631de8cf
Shpend Mula <shmula@hotmail.com>
2021-10-31 14:33:30 +0100

    added input field in editRecipe page

--- src/components/UI/TrashIconButton.js
+++ src/components/UI/TrashIconButton.js
@@ -2,8 +2,9 @@ import TrashIcon from "../../svg/TrashIcon";
 import classes from "./TrashIconButton.module.css";
 
 const TrashIconIconButton = (props) => {
+  const styles = `${classes.btn} ${props.className || ""}`;
   return (
-    <button type='button' className={classes.btn}>
+    <button type='button' className={styles}>
       <TrashIcon className={classes.icon} />
     </button>
   );
--- src/pages/EditRecipe/EditRecipe.js
+++ src/pages/EditRecipe/EditRecipe.js
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import LeftArrowIconButton from "../../components/UI/LeftArrowIconButton";
+import TrashIconButton from "../../components/UI/TrashIconButton";
 import Button from "../../components/UI/Button";
 import classes from "./EditRecipe.module.css";
 
@@ -25,9 +26,25 @@ const EditRecipe = (props) => {
           <LeftArrowIconButton />
         </Link>
         <h1 className={classes.title}>Edit Recipe</h1>
-        <Button className={classes.btn}>Save</Button>
+        <Button className={classes["btn-primary"]}>Save</Button>
       </div>
-      <div className={classes.recipe}></div>
+      <div className={classes.recipe}>
+        <form noValidate>
+          <div className={classes.control}>
+            <label>Title</label>
+            <input type='text' placeholder='Give your recipe a name' />
+          </div>
+          <div className={classes["image-container"]}>
+            <img src='#' alt='describe image' className={classes["recipe-image"]} />
+            <Button className={classes["btn-secondary"]}>Change image</Button>
+            <TrashIconButton className={classes["trash-btn"]} />
+          </div>
+          <div className={classes.control}>
+            <label>Description</label>
+            <textarea placeholder='Introduce your recipe, add notes, cooking tips, serving suggestions, etc...' />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
